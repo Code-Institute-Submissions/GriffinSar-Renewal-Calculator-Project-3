34f35ca409b6d1aba5c435f36009a480c1fb9a29
GriffinSar <caladoniasaz@gmail.com>
2022-09-02 10:32:52 +0000

    Import Rich API, will use to add styling to the app, such as colours, position and tables

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ colorama.init(autoreset=True)
 import gspread 
 from google.oauth2.service_account import Credentials
 import pandas as pd
+from rich.console import Console
+from rich.table import Table 
 
 
 SCOPE = [
